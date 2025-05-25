105d18cf737bf725e2cdcdcdbbf349d86c52b8af
atohanpy <aquainyo@gmail.com>
2025-05-25 16:09:16 +0300

    Update adsoyadil.js

--- api/adsoyadil.js
+++ api/adsoyadil.js
@@ -1,6 +1,13 @@
 export default async function handler(req, res) {
+  const { ad, soyad, il } = req.query;
+
+  if (!ad || !soyad || !il) {
+    return res.status(400).json({ message: "ad, soyad ve il gerekli" });
+  }
+
   try {
-    const r = await fetch("http://api.prymx.fun/apiler/adsoyad.php?ad=roket&soyad=atar&il=bursa");
+    const apiUrl = `http://api.prymx.fun/apiler/adsoyad.php?ad=${ad}&soyad=${soyad}&il=${il}`;
+    const r = await fetch(apiUrl);
     const data = await r.json();
     res.status(200).json(data);
   } catch (e) {
